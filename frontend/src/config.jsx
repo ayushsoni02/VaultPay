ed52da4518f25b2cc0a94027620648ab8bac4cda
Ayush Soni <ayushsoni1021@gmail.com>
2025-12-24 01:14:13 +0530

    modified config

--- frontend/src/config.jsx
+++ frontend/src/config.jsx
@@ -1,2 +1,2 @@
-// export const BACKEND_URL = "https://vaultpay-production.up.railway.app/";
-export const BACKEND_URL = "http://localhost:3000";
+export const BACKEND_URL = "https://vaultpay-production.up.railway.app/";
+// export const BACKEND_URL = "http://localhost:3000";
