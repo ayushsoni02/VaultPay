8a4a3e3bb8ab91304649b565f76fab925242dd4f
Ayush Soni <ayushsoni1021@gmail.com>
2026-01-17 14:46:45 +0530

    update the config file

--- frontend/src/config.jsx
+++ frontend/src/config.jsx
@@ -1,2 +1,12 @@
-export const BACKEND_URL = "https://vaultpay-production.up.railway.app/";
+// export const BACKEND_URL = "https://vaultpay-production.up.railway.app/";
 // export const BACKEND_URL = "http://localhost:3000";
+
+const isDev = import.meta.env.MODE === 'development';
+
+export const BACKEND_URL = isDev
+    ? "http://localhost:3000"
+    : "https://vaultpay-production.up.railway.app/";
+
+export const FRONTEND_URL = isDev
+    ? "http://localhost:5173"
+    : "https://vaultpay-ten.vercel.app/";
